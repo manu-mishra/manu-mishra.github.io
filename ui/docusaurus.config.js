261c5e4114f5565dbaa7996bb55afae1de640638
manu-mishra <mishra.manu@outlook.com>
2025-06-01 11:21:46 -0500

    test change

--- ui/docusaurus.config.js
+++ ui/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Manu Mishra',
-  tagline: 'Hey Friends!!',
+  tagline: 'Hey Friend!!',
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
